f31527a3ac1a7da79c84d0e5791110939bcee71e
cheboi <moscheb96@gmail.com>
2022-10-18 14:12:25 +0300

    some clean up

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let form = document.getElementById("modal-1");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
-// let done = document.getElementsByClassName("doneInput");
 let textarea = document.getElementById("descriptionArea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
@@ -9,48 +8,6 @@ let tasks2 = document.getElementById("tasks2");
 let add = document.getElementById("add");
 let modal = document.querySelector(".modal");
 
-// modal
-(function () {
-  console.log( document.querySelectorAll(".open-modal"))
-  document.querySelectorAll(".open-modal").forEach(function (trigger) {
-    trigger.addEventListener("click", function () {
-      hideAllModalWindows();
-      showModalWindow(this);
-    });
-  });
-
-  document.querySelectorAll(".modal-hide").forEach(function (closeBtn) {
-    closeBtn.addEventListener("click", function () {
-      hideAllModalWindows();
-    });
-  });
-
-  document.querySelector(".modal-fader").addEventListener("click", function () {
-    hideAllModalWindows();
-  });
-})();
-
-function showModalWindow(buttonEl) {
-  var modalTarget = "#" + buttonEl.getAttribute("data-target");
-
-  document.querySelector(".modal-fader").className += " active";
-  document.querySelector(modalTarget).className += " active";
-}
-
-function hideAllModalWindows() {
-  var modalFader = document.querySelector(".modal-fader");
-  var modalWindows = document.querySelectorAll(".modal-window");
-
-  if (modalFader.className.indexOf("active") !== -1) {
-    modalFader.className = modalFader.className.replace("active", "");
-  }
-
-  modalWindows.forEach(function (modalWindow) {
-    if (modalWindow.className.indexOf("active") !== -1) {
-      modalWindow.className = modalWindow.className.replace("active", "");
-    }
-  });
-}
 // todo logic
 
 form.addEventListener("submit", (e) => {
@@ -59,7 +16,7 @@ form.addEventListener("submit", (e) => {
 });
 
 let formValidation = () => {
-  if (textInput.value === "" && textInput.date === "") {
+  if (textInput.value === "") {
     console.log("failure");
     msg.innerHTML = "Task cannot be blank";
   } else {
@@ -76,46 +33,24 @@ let formValidation = () => {
 };
 
 // data to display
-let todoData = [
-  {
-    text: "This is title",
-    description: "title 1 and more",
-    date: "19/10/2022",
-  },
-  {
-    text: "This is second title",
-    description: "I am the second task",
-    date: "02/10/2022",
-  },
-  {
-    text: "This is third title",
-    description: "title 3 and more",
-    date: "18/10/2022",
-  },
-  {
-    text: "This is forth title",
-    description: "title 4 and more",
-    date: "22/02/2022",
-  },
-];
+let todoData = [];
 
-
-let addData = () => {
+const addData = () => {
   todoData.push({
     text: textInput.value,
     description: textarea.value,
     date: dateInput.value,
   });
 
-  localStorage.setItem("data", JSON.stringify(todoData));
+  localStorage.setItem("todoData", JSON.stringify(todoData));
   createTasks();
 };
 
 
-let resetForm = () => {
+const resetForm = () => {
   textInput.value = "";
-  dateInput.value = "";
   textarea.value = "";
+  dateInput.value = "";
 };
 
 
@@ -124,13 +59,12 @@ let createTasks = () => {
   todoData.forEach((x, y) => {
     return (tasks.innerHTML += `
       <li  id=${y} class='task-list'>
-        <input type="checkbox" class="checkBox" onclick="changeListener(this)" >
+        <input type="checkbox" class="checkBox" onClick="changeListener(this)" >
         <span class="title-text">${x.text}</span>
         <br />
-        <span class="date-text">Due date: ${x.date}</span>
-        <br />
-
         <p>${x.description}</p>
+        <br />
+        <span class="date-text">Due date: ${x.date}</span>
         <span class="btn-utils">
           <button onClick= "editTask(this)" class = "open-modal edit" data-target="modal-1"> Edit</button>
           <button onClick ="deleteTask(this);createTasks()" class="btn-delete"> Delete</button>
@@ -141,27 +75,27 @@ let createTasks = () => {
     resetForm();
 };
 
-let deleteTask = (e) => {
-  e.parentElement.parentElement.remove();
-  todoData.splice(e.parentElement.parentElement.id, 1);
-
-  localStorage.setItem("data", JSON.stringify(todoData));
-  // console.log(todoData);
-};
-
 (() => {
-  todoData = JSON.parse(localStorage.getItem("data")) || [];
-  // console.log(todoData);
+  todoData = JSON.parse(localStorage.getItem("todoData")) || [];
   createTasks();
 })();
 
-// edit
-let editTask = (e) => {
-  let selectedTask = e.parentElement.parentElement;
+const deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  todoData.splice(e.parentElement.parentElement.id, 1);
 
-  textInput.value = selectedTask.children[0].innerHTML;
-  dateInput.value = selectedTask.children[1].innerHTML;
-  textarea.value = selectedTask.children[2].innerHTML;
+  localStorage.setItem("todoData", JSON.stringify(todoData));
+  // console.log(todoData);
+};
+
+// edit
+const editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+  console.log(selectedTask)
+
+  textInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[3].innerHTML;
+  dateInput.value = selectedTask.children[4].innerHTML;
 
   deleteTask(e);
 };
@@ -171,17 +105,23 @@ let Check = document.querySelectorAll(".checkBox");
 let completedData = [];
 
 const changeListener = (e) => { 
-  let completedDat = todoData.splice(e.target.parentElement.id, 1);
+  let completedDat = todoData.splice(e.parentElement.id, 1);
   localStorage.setItem("todoData", JSON.stringify(todoData));
   completedData.push(...completedDat)
-  localStorage.setItem("completedItem", JSON.stringify(completedData))
-
-  createTasks()
+  localStorage.setItem("completedData", JSON.stringify(completedData))
+  
   completedTask()
+  createTasks()
 }
 
+const deleteCompletedTask = (e) => {
+  e.parentElement.parentElement.remove();
+  completedData.splice(e.parentElement.parentElement.id, 1);
 
-let completedTask = () => {  
+  localStorage.setItem("completedData", JSON.stringify(completedData));
+};
+
+const completedTask = () => {  
   console.log(completedData)
   tasks2.innerHTML = "";
   completedData.forEach((x, y) => {
@@ -207,10 +147,10 @@ let completedTask = () => {
     <li  id=${y} class='task-list'>
     <span class="warning-dueDate">${checkdueDateDIf()}</span>
     <span class="title-text">${x.text}</span>
-    <span class="date-text">Due date: ${x.date}</span>
     <p>${x.description}</p>
+    <span class="date-text">Due date: ${x.date}</span>
     <span class="btn-utils">
-      <button onClick ="deleteTask(this);createTasks()" class="btn-delete"> Delete</button>
+      <button onClick ="deleteCompletedTask(this);createTasks()" class="btn-delete"> Delete</button>
     </span>
   </li>
     `);
@@ -218,5 +158,10 @@ let completedTask = () => {
    resetForm();
 };
 
+
+(() => {
+  completedData = JSON.parse(localStorage.getItem("completedData")) || [];
+  completedTask();
+})();
 
 
